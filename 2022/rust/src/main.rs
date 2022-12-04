1f91927673fb32c9179991ab31a5ed18273902e7
Craig J Perry <craigp84@gmail.com>
2022-12-04 18:28:54 +0000

    rust: Day 1 Part 2 solution

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,5 +1,6 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter, Error, Write};
+use itertools::Itertools;
 
 fn main() -> Result<(), Error> {
     let stdout = stdout();
@@ -10,7 +11,8 @@ fn main() -> Result<(), Error> {
     say(message.as_bytes(), width, &mut writer)?;
     writer.flush()?;
 
-    println!("Day 1, max calories: {}", day1(load_input(1)));
+    println!("Day 1 Part 1, max calories: {}", day1p1(load_input(1)));
+    println!( "Day 1 Part 2, total calories from top 3 elves: {}", day1p2(load_input(1)));
 
     Ok(())
 }
@@ -20,7 +22,7 @@ fn load_input(day: u8) -> String {
     std::fs::read_to_string(path).expect("Unable to read file")
 }
 
-fn day1(input: String) -> i32 {
+fn day1p1(input: String) -> i32 {
     input
         .split("\n\n")
         .map(|batch|
@@ -31,4 +33,18 @@ fn day1(input: String) -> i32 {
         )
         .max()
         .expect("No batches parsed")
+}
+
+fn day1p2(input: String) -> i32 {
+    input
+        .split("\n\n")
+        .map(|batch|
+            batch
+                .lines()
+                .map(|num| num.parse::<i32>().unwrap())
+                .sum::<i32>()
+        )
+        .sorted_by(|a, b| b.cmp(a))
+        .take(3)
+        .sum()
 }

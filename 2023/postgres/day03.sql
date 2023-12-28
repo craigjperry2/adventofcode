062f8eed8272b6dcb1ada6958d1c9aea51a368fb
Craig J Perry <craigp84@gmail.com>
2023-12-28 21:53:01 +0000

    day3: fix ordering

--- 2023/postgres/day03.sql
+++ 2023/postgres/day03.sql
@@ -2,9 +2,10 @@
 
 -- Load the staging table
 CREATE TABLE day3 (
+    y serial primary key,
     line text not null CHECK ( char_length(line) >= 0 )
 );
-\COPY day3 FROM PROGRAM 'cat day03.txt';
+\COPY day3(line) FROM PROGRAM 'cat day03.txt';
 
 
 -- How long is a line?
@@ -18,30 +19,6 @@ select count(*) from day3;
 
 -- Attempt the solution in parts by gradually building upon CTEs
 
--- Enumerate the lines of raw import day3 table, e.g. add the y column:
---    y | line
---   --------------------
---    1 | '...123...&...'
---    2 | '.45....67...%'
-WITH enumerated AS (
-    SELECT row_number() over () AS y, line
-    FROM day3
-),
-
--- Split the lines into rows:
---  y | n
--- ---------
---  1 | .
---  1 | .
---  1 | .
---  1 | '1'
---  1 | '2'
---  1 | '3'
-rows AS (
-    SELECT y, regexp_split_to_table(line, '') AS n
-    FROM enumerated
-),
-
 -- Enumerate the column of each n:
 --  y | x | n
 -- -------------
@@ -51,9 +28,9 @@ rows AS (
 --  1 | 4 | '1'
 --  1 | 5 | '2'
 --  1 | 6 | '3'
-cols AS (
-    SELECT y, row_number() over (partition by y) AS x, n
-    FROM rows
+WITH cols AS (
+    SELECT y, row_number() over (partition by y order by y) AS x, n
+    FROM day3, regexp_split_to_table(line, '') AS n
 ),
 
 -- Strip out the '.' locations:
@@ -175,27 +152,19 @@ offsets as (
 -- Use the starting x,y and length to parse out the part numbers
 part_numbers as (
     select o.*, substring (line from o.x for o.len)::int as part
-    from offsets o, enumerated e
+    from offsets o, day3 e
     where o.y = e.y
 )
-
+-- select * from cols;
 -- Calculate answer for star 1
 select sum(part) from part_numbers;
 
 
 --[ Part 2 ]-------------------------------------------------------------------
 
-WITH enumerated AS (
-    SELECT row_number() over () AS y, line
-    FROM day3
-),
-rows AS (
-    SELECT y, regexp_split_to_table(line, '') AS n
-    FROM enumerated
-),
-cols AS (
-    SELECT y, row_number() over (partition by y) AS x, n
-    FROM rows
+WITH cols AS (
+    SELECT y, row_number() over (partition by y order by y) AS x, n
+    FROM day3, regexp_split_to_table(line, '') AS n
 ),
 locations as (
     select y, x, n
@@ -259,15 +228,12 @@ offsets_gears as (
     having array_length(array_agg(l.x), 1) = 2
 ),
 gear_ratios as (
-    select part1, part2
-    from (
-        select
-            substring (e1.line from o.x[1] for o.len[1])::int as part1,
-            substring (e2.line from o.x[2] for o.len[2])::int as part2
-        from offsets_gears o, enumerated e1, enumerated e2
-        where o.y[1] = e1.y
-        and o.y[2] = e2.y
-    )
+    select
+        substring (e1.line from o.x[1] for o.len[1])::int as part1,
+        substring (e2.line from o.x[2] for o.len[2])::int as part2
+    from offsets_gears o, day3 e1, day3 e2
+    where o.y[1] = e1.y
+    and o.y[2] = e2.y
 ),
 part_numbers as (
     select part1 * part2 as ratio

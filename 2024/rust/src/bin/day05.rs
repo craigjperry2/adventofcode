949ccc5b29356c971e2829e7c2c6037b6e5d03e9
Craig J Perry <craigp84@gmail.com>
2024-12-05 13:38:34 +0000

    day 5 part 2

--- 2024/rust/src/bin/day05.rs
+++ 2024/rust/src/bin/day05.rs
@@ -30,7 +30,43 @@ fn main() {
         })
         .sum();
 
-    println!("{:?}", result);
+    println!("Part 1: {:?}", result);
+
+    // --------------------  PART 2 --------------------
+
+    let result: i32 = updates
+        .lines()
+        .map(|l| l.split(',').map(str::to_string).collect())
+        .filter(|u| !in_order(&parsed_rules, u))
+        .map(|u| put_in_order(&parsed_rules, u))
+        .map(|u| {
+            u[u.len().div(2)]
+                .parse::<i32>()
+                .unwrap_or_else(|e| panic!("Failed to parse {} as i32", e))
+        })
+        .sum();
+
+    println!("Part 2: {:?}", result);
+}
+
+fn put_in_order(rules: &HashMap<String, HashSet<String>>, update: Vec<String>) -> Vec<String> {
+    let mut result = update.clone();
+    for i in 0..result.len() {
+        let earlier = rules
+            .get(&update[i])
+            .iter()
+            .flat_map(|h| h.iter())
+            .map(|r| result.iter().position(|u| u == r))
+            .flatten()
+            .min()
+            .filter(|e| *e < i);
+
+        if let Some(e) = earlier {
+            result.remove(i);
+            result.insert(e, update[i].clone());
+        }
+    }
+    result
 }
 
 fn in_order(rules: &HashMap<String, HashSet<String>>, update: &Vec<String>) -> bool {
